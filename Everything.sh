c3f4c4f24936ff29067cc9d9e5b702bba909e0f8
ZonicTrout <117415204+ZonicTrout@users.noreply.github.com>
2023-02-05 18:13:12 -0800

    MC install
    Changed from tar.gz to .deb MC file

--- Everything.sh
+++ Everything.sh
@@ -9,8 +9,6 @@ sudo apt-get g++
 sudo apt-get pluma
 sudo apt-get git
 git clone https://github.com/ZonicTrout/Snake-ish.git
-curl https://launcher.mojang.com/download/Minecraft.tar.gz -o Minecraft.tar.gz
-tar -xvf Minecraft.tar.gz
-rm Minecraft.tar.gz
-cd minecraft-launcher
-./minecraft-launcher
+curl https://launcher.mojang.com/download/Minecraft.deb
+sudo apt-get install ./Minecraft.deb
+rm Minecraft.deb
