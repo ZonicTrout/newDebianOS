9b051e29bde44ebe35f18b33eaf19a3c8e5ce7b0
ZonicTrout <117415204+ZonicTrout@users.noreply.github.com>
2023-02-09 21:39:59 -0800

    fix

--- Everything.sh
+++ Everything.sh
@@ -7,7 +7,7 @@ sudo apt-get install g++
 sudo apt-get install pluma
 sudo apt-get install git
 sudo snap install spotify --classic
-sudo snap install code
+sudo snap install code --classic
 git clone https://github.com/ZonicTrout/Snake-ish.git
 $ wget ~/Minecraft.deb https://launcher.mojang.com/download/Minecraft.deb -o Minecraft.deb
 sudo apt-get install ./Minecraft.deb
