db131aafa53b0f834af6ce6f3afe02b510151791
ZonicTrout <117415204+ZonicTrout@users.noreply.github.com>
2023-02-21 16:32:52 -0800

    Adding expressvpn

--- Everything.sh
+++ Everything.sh
@@ -6,9 +6,35 @@ sudo apt-get install gcc
 sudo apt-get install g++
 sudo apt-get install pluma
 sudo apt-get install git
+sudo apt-get install curl
+echo "Done with most apt installs"
+
 sudo snap install spotify --classic
 sudo snap install code --classic
+echo "Done with snap installs"
+
+curl https://www.expressvpn.works/clients/linux/expressvpn_3.43.0.4-1_amd64.deb -o express.deb
+sudo apt install ./express.deb
+
+echo "Done with Expressvpn installs, now activation"
+
+echo "Get Code from https://www.expressvpn.com/subscriptions/"
+sudo expressvpn activate
+
+echo "Done with expressvpn activation"
+
 git clone https://github.com/ZonicTrout/Snake-ish.git
-$ wget ~/Minecraft.deb https://launcher.mojang.com/download/Minecraft.deb -o Minecraft.deb
+
+echo "Done with downloading snake"
+
+curl https://launcher.mojang.com/download/Minecraft.deb -o Minecraft.deb
+
+echo "Done With Minecraft installation, now running setup"
+
 sudo apt-get install ./Minecraft.deb
+
+echo "Done with Minecraft"
+
+echo "Done"
+
 
